d0320d77de3ebedd35bef9dc7ad06e9e8282ee70
Nguyen Trong Minh <93380914+Minhsuperfragile@users.noreply.github.com>
2023-01-08 22:34:01 +0700

    Update introVideo.py

--- introVideo.py
+++ introVideo.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 import pygame
-
+# currently doesn't have that video below uploaded
 clip = VideoFileClip('assets/introVideo.mp4')
 clip = clip.subclip(0,10)
 clip = clip.set_fps(24)
